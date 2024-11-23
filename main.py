3136029ee4a17042ffd7f4ab9f6a0ca362b9d567
BLR <bruff@Macky-Max.local>
2024-11-22 22:32:34 -0500

    this function converts fahrenheit to celsius

---
+++ main.py
@@ -0,0 +1,15 @@
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+"""Converts Celsius to Fahrenheit.
+Args:
+    celsius: the first sumand.
+    9/5: fraction
+    32: number.
+Returns:
+    The result of the conversion from celsius to fahrenheit.
+"""
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
+"""Converts Fahrenheit to Celsius."""
