da7a2485eecf01fe02be293d4509db75f56b3b4c
BLR <bruff@Macky-Max.local>
2024-11-22 22:46:16 -0500

    This function determines if the number is even or odd

--- main.py
+++ main.py
@@ -11,5 +11,20 @@ Returns:
 
 def fahrenheit_to_celsius(fahrenheit):
     return (fahrenheit - 32) * 5/9
+"""Converts Fahrenheit to Celsius.
+Args:
+    fahrenheit: the first sumand.
+    32: number 
+    5/9: fraction
+Returns:
+    The result of the conversion from fahrenheit to celsius.
+"""
 
-"""Converts Fahrenheit to Celsius."""
+def is_even(num):
+    return num % 2 == 0
+"""Finds out if the number is even or odd
+Args:
+    Even or Odd
+Returns:
+    The result of the number is even or odd.
+"""
